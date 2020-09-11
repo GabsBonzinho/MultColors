90816982af0e36a5e3f3f49cafa8c38465dd8a95
Gabriel Marques de Alcantara <51837368+GabsBonzinho@users.noreply.github.com>
2020-09-10 22:28:05 -0300

    Projeto MultiColors

---
+++ PrjColorBall/PrjColorBall/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace PrjColorBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listacores = new System.Windows.Forms.RichTextBox();
+            this.Quadrado001 = new System.Windows.Forms.Panel();
+            this.Quadrado002 = new System.Windows.Forms.Panel();
+            this.Quadrado003 = new System.Windows.Forms.Panel();
+            this.Quadrado004 = new System.Windows.Forms.Panel();
+            this.Quadrado005 = new System.Windows.Forms.Panel();
+            this.Quadrado006 = new System.Windows.Forms.Panel();
+            this.Bola05 = new System.Windows.Forms.Panel();
+            this.Bola04 = new System.Windows.Forms.Panel();
+            this.Bola03 = new System.Windows.Forms.Panel();
+            this.Bola02 = new System.Windows.Forms.Panel();
+            this.Bola01 = new System.Windows.Forms.Panel();
+            this.Bola10 = new System.Windows.Forms.Panel();
+            this.Bola09 = new System.Windows.Forms.Panel();
+            this.Bola08 = new System.Windows.Forms.Panel();
+            this.Bola07 = new System.Windows.Forms.Panel();
+            this.Bola06 = new System.Windows.Forms.Panel();
+            this.Bola15 = new System.Windows.Forms.Panel();
+            this.Bola14 = new System.Windows.Forms.Panel();
+            this.Bola13 = new System.Windows.Forms.Panel();
+            this.Bola12 = new System.Windows.Forms.Panel();
+            this.Bola11 = new System.Windows.Forms.Panel();
+            this.Bola20 = new System.Windows.Forms.Panel();
+            this.Bola19 = new System.Windows.Forms.Panel();
+            this.Bola18 = new System.Windows.Forms.Panel();
+            this.Bola17 = new System.Windows.Forms.Panel();
+            this.Bola16 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inicio do Jogo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listacores
+            // 
+            this.listacores.Location = new System.Drawing.Point(161, 12);
+            this.listacores.Name = "listacores";
+            this.listacores.Size = new System.Drawing.Size(325, 37);
+            this.listacores.TabIndex = 1;
+            this.listacores.Text = "";
+            // 
+            // Quadrado001
+            // 
+            this.Quadrado001.Location = new System.Drawing.Point(60, 143);
+            this.Quadrado001.Name = "Quadrado001";
+            this.Quadrado001.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado001.TabIndex = 2;
+            // 
+            // Quadrado002
+            // 
+            this.Quadrado002.Location = new System.Drawing.Point(115, 143);
+            this.Quadrado002.Name = "Quadrado002";
+            this.Quadrado002.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado002.TabIndex = 3;
+            // 
+            // Quadrado003
+            // 
+            this.Quadrado003.Location = new System.Drawing.Point(170, 143);
+            this.Quadrado003.Name = "Quadrado003";
+            this.Quadrado003.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado003.TabIndex = 4;
+            // 
+            // Quadrado004
+            // 
+            this.Quadrado004.Location = new System.Drawing.Point(225, 143);
+            this.Quadrado004.Name = "Quadrado004";
+            this.Quadrado004.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado004.TabIndex = 5;
+            // 
+            // Quadrado005
+            // 
+            this.Quadrado005.Location = new System.Drawing.Point(280, 143);
+            this.Quadrado005.Name = "Quadrado005";
+            this.Quadrado005.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado005.TabIndex = 6;
+            // 
+            // Quadrado006
+            // 
+            this.Quadrado006.Location = new System.Drawing.Point(335, 143);
+            this.Quadrado006.Name = "Quadrado006";
+            this.Quadrado006.Size = new System.Drawing.Size(49, 51);
+            this.Quadrado006.TabIndex = 7;
+            // 
+            // Bola05
+            // 
+            this.Bola05.Location = new System.Drawing.Point(200, 107);
+            this.Bola05.Name = "Bola05";
+            this.Bola05.Size = new System.Drawing.Size(29, 20);
+            this.Bola05.TabIndex = 12;
+            // 
+            // Bola04
+            // 
+            this.Bola04.Location = new System.Drawing.Point(165, 107);
+            this.Bola04.Name = "Bola04";
+            this.Bola04.Size = new System.Drawing.Size(29, 20);
+            this.Bola04.TabIndex = 11;
+            // 
+            // Bola03
+            // 
+            this.Bola03.Location = new System.Drawing.Point(130, 107);
+            this.Bola03.Name = "Bola03";
+            this.Bola03.Size = new System.Drawing.Size(29, 20);
+            this.Bola03.TabIndex = 10;
+            // 
+            // Bola02
+            // 
+            this.Bola02.Location = new System.Drawing.Point(95, 107);
+            this.Bola02.Name = "Bola02";
+            this.Bola02.Size = new System.Drawing.Size(29, 20);
+            this.Bola02.TabIndex = 9;
+            // 
+            // Bola01
+            // 
+            this.Bola01.Location = new System.Drawing.Point(60, 107);
+            this.Bola01.Name = "Bola01";
+            this.Bola01.Size = new System.Drawing.Size(29, 20);
+            this.Bola01.TabIndex = 8;
+            // 
+            // Bola10
+            // 
+            this.Bola10.Location = new System.Drawing.Point(375, 107);
+            this.Bola10.Name = "Bola10";
+            this.Bola10.Size = new System.Drawing.Size(29, 20);
+            this.Bola10.TabIndex = 17;
+            // 
+            // Bola09
+            // 
+            this.Bola09.Location = new System.Drawing.Point(340, 107);
+            this.Bola09.Name = "Bola09";
+            this.Bola09.Size = new System.Drawing.Size(29, 20);
+            this.Bola09.TabIndex = 16;
+            // 
+            // Bola08
+            // 
+            this.Bola08.Location = new System.Drawing.Point(305, 107);
+            this.Bola08.Name = "Bola08";
+            this.Bola08.Size = new System.Drawing.Size(29, 20);
+            this.Bola08.TabIndex = 15;
+            // 
+            // Bola07
+            // 
+            this.Bola07.Location = new System.Drawing.Point(270, 107);
+            this.Bola07.Name = "Bola07";
+            this.Bola07.Size = new System.Drawing.Size(29, 20);
+            this.Bola07.TabIndex = 14;
+            // 
+            // Bola06
+            // 
+            this.Bola06.Location = new System.Drawing.Point(235, 107);
+            this.Bola06.Name = "Bola06";
+            this.Bola06.Size = new System.Drawing.Size(29, 20);
+            this.Bola06.TabIndex = 13;
+            // 
+            // Bola15
+            // 
+            this.Bola15.Location = new System.Drawing.Point(550, 107);
+            this.Bola15.Name = "Bola15";
+            this.Bola15.Size = new System.Drawing.Size(29, 20);
+            this.Bola15.TabIndex = 17;
+            // 
+            // Bola14
+            // 
+            this.Bola14.Location = new System.Drawing.Point(515, 107);
+            this.Bola14.Name = "Bola14";
+            this.Bola14.Size = new System.Drawing.Size(29, 20);
+            this.Bola14.TabIndex = 16;
+            // 
+            // Bola13
+            // 
+            this.Bola13.Location = new System.Drawing.Point(480, 107);
+            this.Bola13.Name = "Bola13";
+            this.Bola13.Size = new System.Drawing.Size(29, 20);
+            this.Bola13.TabIndex = 15;
+            // 
+            // Bola12
+            // 
+            this.Bola12.Location = new System.Drawing.Point(445, 107);
+            this.Bola12.Name = "Bola12";
+            this.Bola12.Size = new System.Drawing.Size(29, 20);
+            this.Bola12.TabIndex = 14;
+            // 
+            // Bola11
+            // 
+            this.Bola11.Location = new System.Drawing.Point(410, 107);
+            this.Bola11.Name = "Bola11";
+            this.Bola11.Size = new System.Drawing.Size(29, 20);
+            this.Bola11.TabIndex = 13;
+            // 
+            // Bola20
+            // 
+            this.Bola20.Location = new System.Drawing.Point(725, 107);
+            this.Bola20.Name = "Bola20";
+            this.Bola20.Size = new System.Drawing.Size(29, 20);
+            this.Bola20.TabIndex = 17;
+            this.Bola20.Paint += new System.Windows.Forms.PaintEventHandler(this.Bola20_Paint);
+            // 
+            // Bola19
+            // 
+            this.Bola19.Location = new System.Drawing.Point(690, 107);
+            this.Bola19.Name = "Bola19";
+            this.Bola19.Size = new System.Drawing.Size(29, 20);
+            this.Bola19.TabIndex = 16;
+            this.Bola19.Paint += new System.Windows.Forms.PaintEventHandler(this.Bola19_Paint);
+            // 
+            // Bola18
+            // 
+            this.Bola18.Location = new System.Drawing.Point(655, 107);
+            this.Bola18.Name = "Bola18";
+            this.Bola18.Size = new System.Drawing.Size(29, 20);
+            this.Bola18.TabIndex = 15;
+            this.Bola18.Paint += new System.Windows.Forms.PaintEventHandler(this.Bola18_Paint);
+            // 
+            // Bola17
+            // 
+            this.Bola17.Location = new System.Drawing.Point(620, 107);
+            this.Bola17.Name = "Bola17";
+            this.Bola17.Size = new System.Drawing.Size(29, 20);
+            this.Bola17.TabIndex = 14;
+            this.Bola17.Paint += new System.Windows.Forms.PaintEventHandler(this.Bola17_Paint);
+            // 
+            // Bola16
+            // 
+            this.Bola16.Location = new System.Drawing.Point(585, 107);
+            this.Bola16.Name = "Bola16";
+            this.Bola16.Size = new System.Drawing.Size(29, 20);
+            this.Bola16.TabIndex = 13;
+            this.Bola16.Paint += new System.Windows.Forms.PaintEventHandler(this.Bola16_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Bola20);
+            this.Controls.Add(this.Bola15);
+            this.Controls.Add(this.Bola19);
+            this.Controls.Add(this.Bola10);
+            this.Controls.Add(this.Bola18);
+            this.Controls.Add(this.Bola14);
+            this.Controls.Add(this.Bola17);
+            this.Controls.Add(this.Bola05);
+            this.Controls.Add(this.Bola16);
+            this.Controls.Add(this.Bola13);
+            this.Controls.Add(this.Bola09);
+            this.Controls.Add(this.Bola12);
+            this.Controls.Add(this.Bola04);
+            this.Controls.Add(this.Bola11);
+            this.Controls.Add(this.Bola08);
+            this.Controls.Add(this.Bola03);
+            this.Controls.Add(this.Bola07);
+            this.Controls.Add(this.Bola02);
+            this.Controls.Add(this.Bola06);
+            this.Controls.Add(this.Bola01);
+            this.Controls.Add(this.Quadrado006);
+            this.Controls.Add(this.Quadrado005);
+            this.Controls.Add(this.Quadrado004);
+            this.Controls.Add(this.Quadrado003);
+            this.Controls.Add(this.Quadrado002);
+            this.Controls.Add(this.Quadrado001);
+            this.Controls.Add(this.listacores);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox listacores;
+        private System.Windows.Forms.Panel Quadrado001;
+        private System.Windows.Forms.Panel Quadrado002;
+        private System.Windows.Forms.Panel Quadrado003;
+        private System.Windows.Forms.Panel Quadrado004;
+        private System.Windows.Forms.Panel Quadrado005;
+        private System.Windows.Forms.Panel Quadrado006;
+        private System.Windows.Forms.Panel Bola05;
+        private System.Windows.Forms.Panel Bola04;
+        private System.Windows.Forms.Panel Bola03;
+        private System.Windows.Forms.Panel Bola02;
+        private System.Windows.Forms.Panel Bola01;
+        private System.Windows.Forms.Panel Bola10;
+        private System.Windows.Forms.Panel Bola09;
+        private System.Windows.Forms.Panel Bola08;
+        private System.Windows.Forms.Panel Bola07;
+        private System.Windows.Forms.Panel Bola06;
+        private System.Windows.Forms.Panel Bola15;
+        private System.Windows.Forms.Panel Bola14;
+        private System.Windows.Forms.Panel Bola13;
+        private System.Windows.Forms.Panel Bola12;
+        private System.Windows.Forms.Panel Bola11;
+        private System.Windows.Forms.Panel Bola20;
+        private System.Windows.Forms.Panel Bola19;
+        private System.Windows.Forms.Panel Bola18;
+        private System.Windows.Forms.Panel Bola17;
+        private System.Windows.Forms.Panel Bola16;
+    }
+}
+
---
+++ PrjColorBall/PrjColorBall/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrjColorBall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var contador = 0;
+            var Corzinha = new Color[6];
+            var nomeCorzinha = new String[6];
+            var randomico = new Random();
+            var nomedocomponente = "";
+            var ordenacaobolas = new int[20];
+
+            /// Cria um array com os valores referentes 
+            /// as cores
+            Corzinha[contador] = Color.Red;
+            nomeCorzinha[contador] = "Vermelho";
+            contador += 1;
+            Corzinha[contador] = Color.Green;
+            nomeCorzinha[contador] = "Verde";
+            contador += 1;
+            Corzinha[contador] = Color.Purple;
+            nomeCorzinha[contador] = "Roxo";
+            contador += 1;
+            Corzinha[contador] = Color.Blue;
+            nomeCorzinha[contador] = "Azul";
+            contador += 1;
+            Corzinha[contador] = Color.Yellow;
+            nomeCorzinha[contador] = "Amarelo";
+            contador += 1;
+            Corzinha[contador] = Color.Pink;
+            nomeCorzinha[contador] = "Rosa";
+            
+            for (int i = 0; i < 6; i++)
+            {
+                nomedocomponente = "Quadrado00"+Convert.ToString(i+1); ;
+                (Controls[nomedocomponente] as Panel).BackColor = Corzinha[i];
+            }
+            contador = 0;
+
+            listacores.Text = "";
+            
+            for (int i = 0; i < 20; i++)
+            {
+                if ((i+1) <= 9) 
+                 {
+                    nomedocomponente = "Bola0" + Convert.ToString(i+1);
+                 }
+                else
+                {
+                    nomedocomponente = "Bola" + Convert.ToString(i+1);
+                };
+                ordenacaobolas[i] = randomico.Next(0, 5);
+                (Controls[nomedocomponente] as Panel).BackColor = Corzinha[ordenacaobolas[i]];
+            }
+
+            int vermelho,roxo,verde,azul,amarelo,rosa = 0;
+
+            int caseswitch = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                caseswitch = (ordenacaobolas[i] + 1);
+                switch (caseswitch)
+                {
+                    case 1:
+                        vermelho =+ 1;
+                        break;
+                    case 2: 
+                        verde =+ 1;
+                        break;
+                    case 3: 
+                        roxo =+ 1;
+                        break;
+                    case 4: 
+                        azul =+ 1;
+                        break;
+                    case 5: 
+                        amarelo =+ 1;
+                        break;
+                    case 6: 
+                        rosa =+ 1;
+                        break;
+                }
+            }
+        }
+
+        private string tostring(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Bola19_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Bola18_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Bola17_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Bola20_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Bola16_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
